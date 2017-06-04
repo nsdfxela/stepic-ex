5ad65394f6b6ac84bb62a196ae6ae73d776cb931
nsdfxela <nsdfxela@gmail.com>
2017-06-04 20:05:32 +0300

    initial-commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+std::string derivative (std::string polynomial){
+
+	return (std::string)"";
+}
+
+int main (){
+	std::string test1 = "x^2+x";
+	std::cout << derivative(test1);
+	return 0;
+}
