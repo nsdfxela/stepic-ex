7b979cc1280c0f36a3b72a658f388e6d373b01c7
nsdfxela <nsdfxela@gmail.com>
2017-07-06 21:18:59 +0300

    ex5

---
+++ ex-2-5-chat/client.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+
+int set_non_block (int fd){
+int flags; 
+#if defined (O_NONBLOCK)
+	if(-1==(flags=fcntl(fd, F_GETFL, 0))) {
+		flags = 0;
+	}
+	return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+#else
+	flags = 1;
+	return ioctl(fd, FIOBIO, &flags);
+#endif
+}
+
+int main (int argc, char** argv) {
+	std::cout << "I'am chat client" << std::endl;
+}
