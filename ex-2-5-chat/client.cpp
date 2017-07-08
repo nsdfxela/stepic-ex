b2fc055dc85776f79b6de80fa7b8f8b20c9f0d02
nsdfxela <nsdfxela@gmail.com>
2017-07-08 15:16:09 +0300

    client

--- ex-2-5-chat/client.cpp
+++ ex-2-5-chat/client.cpp
@@ -6,8 +6,10 @@
 #include <fcntl.h>
 #include <sys/epoll.h>
 
+#define MAX_EVENTS 32
+
 int set_nonblock (int fd){
-int flags; 
+int flags;
 #if defined (O_NONBLOCK)
 	if(-1==(flags=fcntl(fd, F_GETFL, 0))) {
 		flags = 0;
@@ -37,6 +39,33 @@ int main (int argc, char** argv) {
 	event.data.fd = masterSocket;
 	event.events = EPOLLIN;
 	epoll_ctl(epoll, EPOLL_CTL_ADD, masterSocket, &event);
+
+	while(true) {
+		epoll_event events[MAX_EVENTS];
+		int N = epoll_wait(epoll, events, MAX_EVENTS, -1);
+		for(int i = 0; i != N; i++) {
+			if(events[i].data.fd == masterSocket){
+				int slaveSocket = accept(masterSocket, 0, 0);
+				set_nonblock(slaveSocket);
+				std::cout << "accept!" << std::endl;
+
+				epoll_event event;
+				event.data.fd = slaveSocket;
+				event.events = EPOLLIN;
+				epoll_ctl(epoll, EPOLL_CTL_ADD, slaveSocket, &event);
+
+			} else {
+				char buffer[1024];
+				int recvResult = recv(events[i].data.fd, buffer, 1024, MSG_NOSIGNAL);
+				if(recvResult == 0 && errno != EAGAIN) {
+					shutdown(events[i].data.fd, SHUT_RDWR);
+					close(events[i].data.fd);
+				} else {
+					printf("%s", buffer);
+				}
+			}
+		}
+	}
 	std::cout << "I'am chat client" << std::endl;
 
 }
