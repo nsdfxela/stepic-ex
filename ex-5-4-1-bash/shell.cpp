f2cee6457e35f32d16667c92ecd9b7e2588da647
nsdfxela <nsdfxela@gmail.com>
2017-08-23 15:13:24 +0300

    upworks

--- ex-5-4-1-bash/shell.cpp
+++ ex-5-4-1-bash/shell.cpp
@@ -1,4 +1,5 @@
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -10,63 +11,79 @@ class cmd {
 public:
 	std::string command;
 	char **params = NULL;
-cmd () { }
-~cmd() { }	
+	cmd () { }
+	~cmd() { }	
 };
 
+#define CLOSEALL()  \
+for(int i = 0; i < commands.size(); i++) { \
+		close (pfds[i][0]); \
+		close (pfds[i][1]); \
+	}\
 
 // cat t1 | grep 2015 | sort
 void create_pipe(std::vector <cmd> &commands) {
 	int pfds [commands.size()][2];
-	int file = open("./res.out", O_WRONLY | O_CREAT, 0666);
 	
-	int *n_pfd_1  = pfds[0];
-	int *n_pfd_2  = pfds[1];
+	
+	for(int i = 1; i < commands.size(); i++) {
+		pipe(pfds[i]);
+	}
+	
 	
 	for(int i = 0; i < commands.size(); i++) {
 	
-	
-	pipe(n_pfd_1);
 	printf("cycle %d \n", i);
-		if(!fork()) {			
-			if(!i) {
-				printf("thread 0 \n");	
+		if(!fork()) {	
+			
+			if(i == 0) {				
+				printf("thread 0 command: %s \n", commands[i].command.c_str());	
 				close(STDOUT_FILENO);
-				dup2(n_pfd_1[1], STDOUT_FILENO);
-				close(n_pfd_1[0]);				
-				close(n_pfd_1[1]);
+				dup2(pfds[i+1][1], STDOUT_FILENO);
+				CLOSEALL();		
+				/*close(pfds[i+1][1]);				
+				close(pfds[i+1][0]);*/
+				fprintf(stderr, "process %d is going to exec \n", i);
 				execvp(commands[i].command.c_str(), commands[i].params);
 			} else {
+				
 				bool last = (i == (commands.size() - 1) );
-				printf("thread %d \n", i+1);	
+				int file;
+				if(last) {
+					file = open("./res.out", O_WRONLY | O_CREAT, 0666);
+				}
+				printf("thread %d command: %s last: %d \n", i, commands[i].command.c_str(), (int)last);	
 			
 				close(STDIN_FILENO);
 				close(STDOUT_FILENO);
 			
-				dup2(n_pfd_1[0], STDIN_FILENO);			
-				dup2( last ? file : n_pfd_2[1], STDOUT_FILENO);
-				close(n_pfd_1[1]);
-				close(n_pfd_1[0]);
-				close(n_pfd_2[0]);
-				close(n_pfd_2[1]);
-				if(last) {
+				dup2(pfds[i][0], STDIN_FILENO);			
+				dup2( last ? file : pfds[i+1][1], STDOUT_FILENO);
+				
+				if(last) {				
+					close(pfds[i][0]);
+					close(pfds[i][1]);
+				} else {
+					CLOSEALL();		
+					/*close(pfds[i+1][0]);
+					close(pfds[i+1][1]); */
+				}
+				/*if(last) {
 					close(file); 
 				} else { 
 					pipe(n_pfd_2); 
-					n_pfd_1 = pfds[i];
-					n_pfd_2 = pfds[i + 1];
+					
+				}*/
+				if(last) {
+					close(file); 					
 				}
-				execvp(commands[i].command.c_str(), commands[i].params);						
+				
+				fprintf(stderr, "process %d is going to exec \n", i);
+				execvp(commands[i].command.c_str(), commands[i].params);
+				
 			}
-		} else {
-			printf("thread %d breaking \n", i);	
-			/*close(STDOUT_FILENO);
-			dup2(n_pfd_1[1], STDOUT_FILENO);
-			close(n_pfd_1[1]);
-			close(n_pfd_1[0]);
-			execvp(commands[i].command.c_str(), commands[i].params);*/						
 			break;
-		}
+		} 
 	}
 	
 
@@ -135,7 +152,7 @@ void create_pipe(std::vector <cmd> &commands) {
 
 // cat t1 | grep 2015 | sort
 void parse_cmd(std::vector <cmd> &pipe) {
-	cmd cmd1, cmd2, cmd3;
+	cmd cmd1, cmd2, cmd3, cmd4;
 	
 	cmd1.command = "cat";
 	cmd1.params = new char *[2];
@@ -153,12 +170,14 @@ void parse_cmd(std::vector <cmd> &pipe) {
 	strcpy(cmd2.params[1], "2015\0");	
 	pipe.push_back(cmd2);
 	
+	
 	cmd3.command = "sort";
 	cmd3.params = new char *[1];
 	cmd3.params[0] = new char[5];
 	strcpy(cmd3.params[0], "sort\0");		
 	pipe.push_back(cmd3);
 	
+
 	/*pipe.push_back("grep 2015");
 	pipe.push_back("sort");*/
 }
